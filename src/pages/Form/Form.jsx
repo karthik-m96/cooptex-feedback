26b5b8dcd0f22a1f7812ecc5796b48885e81dac0
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-05-28 09:30:01 +0530

    fixed

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-=======
-import Phone from "../../components/Phone/Phone";
->>>>>>> e87a5a36ec9a85285732253ad1847851caad8833
 import "./Form.scss"
 import Phone from "../../components/Phone/Phone";
 
@@ -10,12 +6,6 @@ const Form = () => {
   return (
     <div className="form">
       <form className="container">
-<<<<<<< HEAD
-        
-=======
-        <label htmlFor=""></label>
-        <input type="text" />
->>>>>>> e87a5a36ec9a85285732253ad1847851caad8833
         <Phone />
         <button type="submit">Submit</button>
       </form>
